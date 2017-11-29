211fb653449a9f4bfc62e61329a1e2a86b6330db
quzengpeng <1170650842@qq.com>
2017-11-29 23:16:33 +0800

    关于用户的相关方法

--- src/main/java/com/dapeng/sysuser/controller/SysUserController.java
+++ src/main/java/com/dapeng/sysuser/controller/SysUserController.java
@@ -2,6 +2,8 @@ package com.dapeng.sysuser.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.dapeng.sysuser.service.SysUserService;
 
@@ -11,5 +13,27 @@ public class SysUserController {
 	@Autowired
 	private SysUserService sysUserService;
 	
+	/**
+	 * 登陆
+	 */
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public void login(){
+		
+	}
 	
+	/**
+	 * 注册
+	 */
+	@RequestMapping(value="/register",method=RequestMethod.POST)
+	public void register(){
+		
+	}
+	
+	/**
+	 * 退出登陆
+	 */
+	@RequestMapping(value="/logOut",method=RequestMethod.POST)
+	public void logOut(){
+		
+	}
 }
